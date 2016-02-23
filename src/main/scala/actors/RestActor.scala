23bd05bcb3d2b7ec4b280bcca49dd560fb29426f
Thiago Costa <thiago.costa@groupon.com>
2016-02-23 16:58:53 -0300

    inital api

---
+++ src/main/scala/actors/RestActor.scala
@@ -0,0 +1,28 @@
+package actors
+
+import akka.actor.Actor
+import akka.util.Timeout
+import scala.concurrent.duration._
+import spray.http.HttpHeaders.RawHeader
+import spray.http.MediaTypes._
+import spray.routing.HttpServiceActor
+
+/**
+  * Created by thiago on 2/23/16.
+  */
+
+class RestActor extends HttpServiceActor {
+
+  def receive =  runRoute(route)
+
+  val route = {
+    path("") {
+      get {
+        respondWithMediaType(`application/json`){
+          println("HOLA")
+          complete("{\"bla\": \"ble\"}")
+        }
+      }
+    }
+  }
+}
