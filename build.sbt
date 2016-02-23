ab6b1f82eb7eb24cef4188b3ff8b3602e21f72a0
Thiago Costa <thiago.costa@groupon.com>
2016-02-23 16:10:16 -0300

    inital commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "deadpool_server"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ src/main/scala/DeadPoolServer.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by thiago on 2/23/16.
+  */
+object DeadPoolServer extends App {
+
+  println("CHIMICHANGA");
+
+}
