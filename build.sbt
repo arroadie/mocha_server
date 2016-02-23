9b7ea3150b7ae4c4242a538cfed7867dd0e9ef2b
Thiago Costa <thiago.costa@groupon.com>
2016-02-23 16:58:48 -0300

    inital api

--- build.sbt
+++ build.sbt
@@ -3,4 +3,9 @@ name := "deadpool_server"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.4.2"
+libraryDependencies += "io.spray" % "spray-can_2.11" % "1.3.3"
+libraryDependencies += "io.spray" % "spray-routing_2.11" % "1.3.3"
+libraryDependencies += "io.spray" % "spray-client_2.11" % "1.3.3"
+libraryDependencies += "io.spray" % "spray-json_2.10" % "1.3.2"
--- src/main/scala/DeadPoolServer.scala
+++ src/main/scala/DeadPoolServer.scala
@@ -1,8 +1,25 @@
-/**
-  * Created by thiago on 2/23/16.
-  */
+package deadpool
+
+import actors.RestActor
+import akka.actor.ActorSystem
+import scala.concurrent.duration._
+import akka.io.IO
+import akka.pattern._
+import akka.actor._
+import akka.util.Timeout
+
+import spray.can.Http
+
 object DeadPoolServer extends App {
 
+  implicit val timeout = Timeout(5 seconds)
+
+  implicit val system = ActorSystem("DeadPool-Server")
+
+  val rest = system.actorOf(Props(classOf[RestActor]))
+
   println("CHIMICHANGA");
+
+  IO(Http) ! Http.Bind(rest, "0.0.0.0", 8080)
 
 }
